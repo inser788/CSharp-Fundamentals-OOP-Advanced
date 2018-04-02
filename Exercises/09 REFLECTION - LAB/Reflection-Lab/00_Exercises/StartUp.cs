8f9af3e175b40913e3a1d0763b8d210fc262790c
inser788 <inser7@abv.bg>
2018-04-02 23:32:29 +0300

    add new info

---
+++ Exercises/09 REFLECTION - LAB/Reflection-Lab/00_Exercises/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+public class StartUp
+{
+    static void Main()
+    {
+        var type = typeof(TestReflection);
+        //Console.WriteLine(type.FullName);
+        //var interfaces = type.GetInterfaces();
+        //foreach (var inter in interfaces)
+        //{
+        //    Console.WriteLine(inter.FullName);
+        //}
+
+        //var baseType = type.BaseType;
+        //Console.WriteLine(baseType.FullName);
+
+
+        //var constructors=type.GetConstructors();
+
+        //var test = Activator.CreateInstance(typeof(TestReflection),true);
+
+        //var fields = type.GetFields(BindingFlags.Static | BindingFlags.NonPublic |
+        //                            BindingFlags.Public | BindingFlags.Instance);
+
+        var typeBuilder = typeof(StringBuilder);
+        var constructor = typeBuilder.GetConstructor(new Type[] {typeof(string), typeof(int)});
+        var sb = constructor.Invoke(new object[] {"georgi", 100});
+
+
+        Console.WriteLine();
+    }
+}
+
---
+++ Exercises/09 REFLECTION - LAB/Reflection-Lab/00_Exercises/TestReflection.cs
@@ -0,0 +1,40 @@
+﻿public class TestReflection
+{
+
+    private int number;
+    public static string text;
+
+    public string secondText;
+    private static int secondNum;
+
+
+
+    private TestReflection(int number)
+    {
+    }
+
+    private TestReflection()
+    {
+        
+    }
+
+    private TestReflection(string name, double size)
+    {
+
+    }
+}
+
+interface IMy
+{
+    
+}
+
+interface INext
+{
+    
+}
+
+public class NewClass
+{
+    
+}
